603755aa506b313d7a690ccd5e8b28dbbeeb0eb2
sloymad <sloymad@gmail.com>
2020-02-04 00:14:27 +0300

    create first connect and test

---
+++ locators/MainPage.py
@@ -0,0 +1,9 @@
+class MainPage:
+
+    nav_links = "ul.nav > li"
+    promoblock = "swiper-viewport"
+    mainNavbar = "navbar-header"
+    savebutton = "cart"
+    searchField = "input-group"
+    searchButton = "input-group-btn"
+    shoppingCart = "Shopping Cart"
---
+++ locators/ProductPage.py
@@ -0,0 +1,8 @@
+class ProductPage:
+
+    add_to_wish_list = "//button[@data-original-title='Add to Wish List']"
+    likebutton = "btn btn-default"
+    buttoncart = "button-cart"
+
+    class alert:
+        success = "//div[contains(@class, 'alert-success')]"
